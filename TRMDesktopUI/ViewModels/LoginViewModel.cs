4a92c70dea249a7f70d92bf43d75124db878cf2e
Kai Bushby <kaisbushby@gmail.com>
2021-04-15 21:34:50 +1000

    Seperated Web.config with connection strings Now connection string will be strored in the local machine's cstring.config

--- TRMDesktopUI/ViewModels/LoginViewModel.cs
+++ TRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -38,6 +38,35 @@ namespace TRMDesktopUI.ViewModels
             }
         }
 
+        public bool IsErrorVisible
+        {
+            get
+            {
+                bool output = false;
+
+                if (ErrorMessage?.Length > 0)
+                {
+                    output = true;
+                }
+                return output;
+            }
+        }
+
+        private string _errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set
+            {
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => ErrorMessage);
+                NotifyOfPropertyChange(() => IsErrorVisible);
+                
+            }
+        }
+
+
         public bool CanLogin
         {
             get
@@ -55,12 +84,15 @@ namespace TRMDesktopUI.ViewModels
         {
             try
             {
+                ErrorMessage = null;
                 var result = await _apiHelper.Authenticate(UserName, Password);
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                ErrorMessage = ex.Message;
             }
         }
+
+
     }
 }
